af5e7945d26470d12bddd78ffac0d625a86d6c87
听 楼 <2913394404@qq.com>
2020-07-10 16:38:26 +0800

    修改完成

--- TheLongDarkBackupTools/Main.Designer.cs
+++ TheLongDarkBackupTools/Main.Designer.cs
@@ -59,7 +59,6 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "漫漫长夜";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
