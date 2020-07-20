e2322fb3d9a8a8cf705148e04439513544a767dc
听 楼 <2913394404@qq.com>
2020-07-20 09:11:47 +0800

    修改了没法打开git项目页面的bug

--- TheLongDarkBackupTools/Main.Designer.cs
+++ TheLongDarkBackupTools/Main.Designer.cs
@@ -81,6 +81,7 @@ namespace TheLongDarkBackupTools
             this.linkLabel1.TabIndex = 2;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "点这里查看我的工作进度";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // checkBox1
             // 
